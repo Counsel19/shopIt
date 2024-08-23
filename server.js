96fc165548713adfed5607084cb1872e3f595686
CounselJS <okpabicounsel@gmail.com>
2024-08-23 12:59:47 +0100

    Alloed ALl Origin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import "express-async-errors";
 import cors from "cors";
 
 let corsOptions = {
-  origin: "http://localhost:5173",
+  origin: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
